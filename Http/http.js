83442da5cfdf651950daf5105be906eaf2924daa
xieshuang <930571051@qq.com>
2023-03-10 21:31:53 +0800

    feat: http

--- Http/http.js
+++ Http/http.js
@@ -6,7 +6,8 @@ server.on('request',(req,res)=>{
    console.log('some one visis :>> '); //xs
    const url = req.url
    const method = req.method
-   
+   // res.setHeader('Content-Type', 'text/html;  charset=uft-8')
+   res.setHeader('Content-Type','text/plain;charset=utf-8');
    res.end('的怂啊都送啊')
 })
 
