c0adb8ce35a364faa564ed385cf1e775e30925f0
xieshuang <930571051@qq.com>
2023-03-18 13:43:16 +0800

    feat: jwt生成

--- JWT/index.js
+++ JWT/index.js
@@ -2,20 +2,34 @@ const express = require('express')
 const app = express()
 
 
-
+const secretKey = 'time1'
+const { expressjwt } = require("express-jwt");
 // 生成
 const jwt = require('jsonwebtoken')
 // 解析
-const expressJWtT = require('express-jwt')
-app.use(expressJWtT({ secret: secretKey }).unless({path:[/^\/api\//]}))
+app.use(expressjwt({ secret: secretKey,algorithms:['HS256'] }).unless({path:[/^\/api\//]}))
 
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
-const secretKey = 'time1'
 
 app.post('/api/login', (req, res) => {
    const userInfo = req.body
    res.send({
+      message:'登陆成功',
       status: 200,
       token: jwt.sign({ userName: userInfo.userName }, secretKey, { expiresIn: 30 })
    })
+})
+
+app.get('/api/info',(req,res)=>{
+   console.log('req :>> ', req.auth); //xs
+   res.send({
+      status:200,
+      data:req.user
+   })
+})
+
+app.listen('3001',()=>{
+
 })
