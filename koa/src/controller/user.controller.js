200dd7f00f1a1be17d9e2011c56993b926806d8c
xieshuang <930571051@qq.com>
2023-04-04 09:21:08 +0800

    feat: token

--- koa/src/controller/user.controller.js
+++ koa/src/controller/user.controller.js
@@ -2,8 +2,10 @@
  * @Author: xs
  * @Date: 2023-03-31 10:53:07
  */
-const { createUser } = require("../service/user.service");
+const { createUser, getUserInfo } = require("../service/user.service");
 const { registerError } = require("../constant/error.type");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config/default");
 class UserController {
   async register(ctx, next) {
     // 操作数据库
@@ -25,6 +27,20 @@ class UserController {
   async login(ctx, next) {
     const { userName, passWord } = ctx.request.body;
     ctx.body = "登陆成功";
+    // 获取用户信息
+    try {
+      // 返回结果,剔除password
+      const { passWord, ...res } = await getUserInfo({
+        user_name: userName,
+      });
+      ctx.body = {
+        code: 0,
+        message: "登陆成功",
+        result: {
+          token: jwt.sign(res, JWT_SECRET, { expiresIn: "1d" }),
+        },
+      };
+    } catch {}
   }
 }
 
